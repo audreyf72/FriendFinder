273d54923cfca302f95a08de16b58dc2f984f82f
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-07-19 16:36:04 -0700

    First Commit

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsData = [
+    {
+        "name":"Diane",
+        "photo":"https://media.licdn.com/dms/image/C4E03AQE7umTGRuMCyw/profile-displayphoto-shrink_800_800/0?e=1537401600&v=beta&t=lTsXcktfBH4NnA29SUm_gW98aLHmxIN_G9ctFMpeD7E",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+
+    {
+        "name":"Cheris",
+        "photo":"https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/10521313_10203840025150649_8171387198873386985_o.jpg?_nc_cat=0&oh=0dc85343d19b05b800371f1e7a26cd07&oe=5BE11448",
+        "scores":[2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+
+    {
+		name: "Yulanda",
+		photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/25299494_10155959942293659_8058801084847558774_n.jpg?_nc_cat=0&oh=0f7e5cbaeb9e2c62d08d256e4bd587e9&oe=5BC8ABAB",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	}    
+];
+
+module.exports = friendsData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+  // Return all friends found in friends.js as JSON
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    // Receive user details (name, photo, scores)
+    var user = req.body;
+
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    var friendMatchIndex = 0;
+    var minimumDifference = 40;
+
+    // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+    //  whatever the difference is, add to the total difference
+    for(var i = 0; i < friendsData.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friendsData[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friendsData[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      if(totalDifference < minimumDifference) {
+        friendMatchIndex = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    // after finding match, add user to friend array
+    friendsData.push(user);
+
+    // send back to browser the best friend match
+    res.json(friendsData[friendMatchIndex]);
+  });
+};
