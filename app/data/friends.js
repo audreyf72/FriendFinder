0a9d9a0ca6dcdd54bfae98d6f513ccc94a6054f8
audrey <audreyfernandez@verizon.net>
2018-07-19 19:31:36 -0700

    Big Bang Character Add

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,38 @@
 var friendsData = [
     {
-        "name":"Diane",
-        "photo":"https://media.licdn.com/dms/image/C4E03AQE7umTGRuMCyw/profile-displayphoto-shrink_800_800/0?e=1537401600&v=beta&t=lTsXcktfBH4NnA29SUm_gW98aLHmxIN_G9ctFMpeD7E",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "name":"Sheldon Cooper",
+        "photo":"https://the-big-bang-theory.com/images/uploads/5/rs_3463ec66edb459bd1dd.jpg",
+        "scores":[3, 5, 1, 5, 5, 1, 2, 1, 1, 5]
     },
 
     {
-        "name":"Cheris",
-        "photo":"https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/10521313_10203840025150649_8171387198873386985_o.jpg?_nc_cat=0&oh=0dc85343d19b05b800371f1e7a26cd07&oe=5BE11448",
-        "scores":[2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+        "name":"Howard Wolowitz",
+        "photo":"https://the-big-bang-theory.com/images/uploads/1/rs_81898e55982e335f611.jpg",
+        "scores":[5, 5, 5, 1, 3, 3, 5, 1, 3, 5]
     },
 
     {
-		name: "Yulanda",
-		photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/25299494_10155959942293659_8058801084847558774_n.jpg?_nc_cat=0&oh=0f7e5cbaeb9e2c62d08d256e4bd587e9&oe=5BC8ABAB",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+        "name":"Leonard Hofstadter",
+        "photo":"https://the-big-bang-theory.com/images/uploads/2/rs_1365ce5b16123fd10b8.jpg",
+        "scores":[5, 5, 1, 3, 4, 4, 1, 3, 4, 5]
+    },
+
+    {
+        "name":"Raj Koothrappali",
+        "photo":"https://the-big-bang-theory.com/images/uploads/2/rs_35619e852e696869058.jpg",
+        "scores":[4, 3, 1, 1, 3, 1, 3, 1, 5, 5]
+    },
+
+    {
+        "name":"Penny",
+        "photo":"https://the-big-bang-theory.com/images/uploads/3/rs_304c3dd3b937df81cac.jpg",
+        "scores":[4, 1, 3, 1, 1, 5, 1, 5, 3, 1]
+    },
+
+    {
+		name: "Bernadette Rostenkowski",
+		photo: "https://the-big-bang-theory.com/images/uploads/9/rs_80e50786d684d9727.jpg",
+		scores: [5, 1, 2, 1, 2, 3, 1, 2, 4, 1]
 	}    
 ];
 
